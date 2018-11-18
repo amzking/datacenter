98d197dbd0141539839c1d045f0837ab24a71a80
amzking <amzking@foxmail.com>
2018-11-18 15:44:37 +0800

    test update

---
+++ src/main/java/com/ccue/datacenter/server/http/HttpNioServer.java
@@ -0,0 +1,70 @@
+package com.ccue.datacenter.server.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
+
+public class HttpNioServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpNioServer.class);
+
+    /**
+     * 监听的端口
+     */
+    private final int port;
+
+    public HttpNioServer (int port) {
+        this.port = port;
+    }
+
+
+    public void serve() throws InterruptedException {
+
+        EventLoopGroup listenGroup = new NioEventLoopGroup(1);
+        // 默认为线程数的两倍
+        EventLoopGroup processGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+
+            bootstrap.group(listenGroup, processGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>(){
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            // 此处添加handler
+
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture future = bootstrap.bind(new InetSocketAddress(port)).sync();
+            future.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    if (channelFuture.isSuccess()) {
+                        System.out.println("Server is serving");
+                    } else {
+                        System.out.println("Server start failed");
+                        channelFuture.cause().printStackTrace();
+                    }
+                }
+            });
+
+            future.channel().closeFuture().sync();
+
+        } finally {
+            listenGroup.shutdownGracefully();
+            processGroup.shutdownGracefully();
+        }
+
+    }
+
+}
---
+++ src/main/java/com/ccue/datacenter/server/http/Start.java
@@ -0,0 +1,4 @@
+package com.ccue.datacenter.server.http;
+
+public class Start {
+}
