128ef41fbd7e654259b6a62de1878a93ac6afbd7
amzking <amzking@foxmail.com>
2018-11-25 00:24:27 +0800

    change

--- src/main/java/com/ccue/datacenter/core/db/mongo/MongoDBClinet.java
+++ src/main/java/com/ccue/datacenter/core/db/mongo/MongoDBClinet.java
@@ -8,7 +8,6 @@ import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class MongoDBClinet {
 
