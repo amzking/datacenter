e221c707622b9a9e9aac3f2e22739c7ce3a98699
amzking <amzking@foxmail.com>
2018-12-17 22:59:05 +0800

    change

--- src/main/java/com/ccue/datacenter/core/server/http/handler/HttpMessageHandler.java
+++ src/main/java/com/ccue/datacenter/core/server/http/handler/HttpMessageHandler.java
@@ -3,14 +3,17 @@ package com.ccue.datacenter.core.server.http.handler;
 import com.ccue.datacenter.core.server.http.request.HttpIntercepterManager;
 import com.ccue.datacenter.core.server.http.request.HttpRequest;
 import com.ccue.datacenter.core.server.http.response.HttpResponse;
-import com.ccue.datacenter.core.server.http.url.router.IDispatcher;
 import com.ccue.datacenter.core.server.http.url.router.IHttpDispatcher;
 import com.ccue.datacenter.core.server.http.url.router.NettyHttpDispatcher;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
+import static io.netty.handler.codec.rtsp.RtspHeaderNames.CONTENT_LENGTH;
 
 @ChannelHandler.Sharable
 public class HttpMessageHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
@@ -25,29 +28,41 @@ public class HttpMessageHandler extends SimpleChannelInboundHandler<FullHttpRequ
      */
     private IHttpDispatcher dispatcher;
 
+    /**
+     * 拦截时返回默认消息
+     */
+    private static final String DEFAULT_INTERCEPT_MSG = "your request was intercepted!";
+
+    /**
+     * 默认FullHtpResponse
+     */
+    private FullHttpResponse DEFAULT_INTERCEPT_RESPONSE = new DefaultFullHttpResponse(
+            HttpVersion.HTTP_1_1, HttpResponseStatus.OK,
+            Unpooled.wrappedBuffer(DEFAULT_INTERCEPT_MSG.getBytes()));
+
     public HttpMessageHandler() {
         // 默认不拦截
         this.allowIntercept = false;
         this.dispatcher = new NettyHttpDispatcher();
     }
 
-    public HttpMessageHandler(boolean allowIntecept, IHttpDispatcher dispatcher) {
-        this.allowIntercept = allowIntecept;
+    public HttpMessageHandler(boolean allowIntercept, IHttpDispatcher dispatcher) {
+        this.allowIntercept = allowIntercept;
         this.dispatcher = dispatcher;
     }
 
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg) throws Exception {
-        FullHttpResponse response = null;
+
         if (allowIntercept && HttpIntercepterManager.getInstance().intercept(msg)) {
-            // 默认返回
-            // 考虑返回状态码
-            response = null;
+            FullHttpResponse response = DEFAULT_INTERCEPT_RESPONSE;
+            response.headers().set(CONTENT_TYPE, "application/xml");
+            response.headers().setInt(CONTENT_LENGTH, response.content().readableBytes());
             ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
         }
         try {
-            response = dispatcher.dispatch(msg);
+            FullHttpResponse response = dispatcher.dispatch(msg);
             ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
         }catch (Exception e) {
             e.printStackTrace();
