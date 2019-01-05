27f6a87cfda7748223ac312a4340bc086b75b7a7
amzking <amzking@foxmail.com>
2019-01-05 23:22:04 +0800

    change

--- src/main/java/com/ccue/datacenter/core/db/mongo/MongoBaseService.java
+++ src/main/java/com/ccue/datacenter/core/db/mongo/MongoBaseService.java
@@ -1,6 +1,24 @@
 package com.ccue.datacenter.core.db.mongo;
 
-public interface MongoBaseService {
+import java.util.List;
+
+public interface MongoBaseService<T> {
+
+
+    void insert(T t);
+
+    void upsert(T t);
+
+    void delete(T t);
+
+    T query();
+
+    void deleteByQuery(T t);
+
+    void insertAll(List<T> list);
+
+    void upsertAll(List<T> list);
+
 
 
 }
