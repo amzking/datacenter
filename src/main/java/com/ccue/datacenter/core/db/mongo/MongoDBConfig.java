8378c36b2c6ab7fd3e7dab2a517f894d496f5179
amzking <amzking@foxmail.com>
2019-02-18 23:12:12 +0800

    change

---
+++ src/main/java/com/ccue/datacenter/core/db/mongo/MongoDBConfig.java
@@ -0,0 +1,121 @@
+package com.ccue.datacenter.core.db.mongo;
+
+import java.util.List;
+
+/**
+ * @author joking@aliyun.com
+ * @description 此类对应moongo config yaml
+ * @date 2019-02-18
+ */
+public class MongoDBConfig {
+
+    private String userName;
+
+    private String password;
+
+    private List<MongoRepica> servers;
+
+    private Integer maintenanceFrequency;
+
+    private Integer maintenanceInitialDelay;
+
+    private Integer maxConnectionIdleTime;
+
+    private Integer maxConnectionLifeTime;
+
+    private Integer maxSize;
+
+    private Integer minSize;
+
+    private Integer maxWaitQueueSize;
+
+    private Integer maxWaitTime;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<MongoRepica> getServers() {
+        return servers;
+    }
+
+    public void setServers(List<MongoRepica> servers) {
+        this.servers = servers;
+    }
+
+    public Integer getMaintenanceFrequency() {
+        return maintenanceFrequency;
+    }
+
+    public void setMaintenanceFrequency(Integer maintenanceFrequency) {
+        this.maintenanceFrequency = maintenanceFrequency;
+    }
+
+    public Integer getMaintenanceInitialDelay() {
+        return maintenanceInitialDelay;
+    }
+
+    public void setMaintenanceInitialDelay(Integer maintenanceInitialDelay) {
+        this.maintenanceInitialDelay = maintenanceInitialDelay;
+    }
+
+    public Integer getMaxConnectionIdleTime() {
+        return maxConnectionIdleTime;
+    }
+
+    public void setMaxConnectionIdleTime(Integer maxConnectionIdleTime) {
+        this.maxConnectionIdleTime = maxConnectionIdleTime;
+    }
+
+    public Integer getMaxConnectionLifeTime() {
+        return maxConnectionLifeTime;
+    }
+
+    public void setMaxConnectionLifeTime(Integer maxConnectionLifeTime) {
+        this.maxConnectionLifeTime = maxConnectionLifeTime;
+    }
+
+    public Integer getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(Integer maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public Integer getMinSize() {
+        return minSize;
+    }
+
+    public void setMinSize(Integer minSize) {
+        this.minSize = minSize;
+    }
+
+    public Integer getMaxWaitQueueSize() {
+        return maxWaitQueueSize;
+    }
+
+    public void setMaxWaitQueueSize(Integer maxWaitQueueSize) {
+        this.maxWaitQueueSize = maxWaitQueueSize;
+    }
+
+    public Integer getMaxWaitTime() {
+        return maxWaitTime;
+    }
+
+    public void setMaxWaitTime(Integer maxWaitTime) {
+        this.maxWaitTime = maxWaitTime;
+    }
+}
---
+++ src/main/java/com/ccue/datacenter/core/db/mongo/MongoRepica.java
@@ -0,0 +1,27 @@
+package com.ccue.datacenter.core.db.mongo;
+
+/**
+ * @author joking@aliyun.com
+ * @description
+ * @date 2019-02-18
+ */
+public class MongoRepica {
+    private String host;
+    private String port;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+}
