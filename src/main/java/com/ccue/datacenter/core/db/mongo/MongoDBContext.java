1bcd7ebb500024af76dd749ebc47a18938d713aa
amzking <amzking@foxmail.com>
2019-02-20 23:13:31 +0800

    change

--- src/main/java/com/ccue/datacenter/core/db/mongo/MongoDBContext.java
+++ src/main/java/com/ccue/datacenter/core/db/mongo/MongoDBContext.java
@@ -1,6 +1,8 @@
 package com.ccue.datacenter.core.db.mongo;
 
 
+import com.ccue.datacenter.utils.yaml.YamlUtil;
+
 import java.util.List;
 
 /**
@@ -9,14 +11,39 @@ import java.util.List;
  */
 public class MongoDBContext extends AbstractDatabaseContext {
 
-    private final List<MongoRepica> clusters;
+    private final MongoDBConfig config;
+
+    /**
+     * @description: 加载配置上下文
+     * @since: 2019-02-20
+     * @param
+     * @return: void
+     */
+    @Override
+    public void init() {
+        MongoDBClinetHolder.initContext();
+    }
 
     public static class Builder extends AbstractDatabaseContext.Builder<Builder> {
-        private List<MongoRepica> clusters;
-        public Builder clusters(List<MongoRepica> clusters) {
-            this.clusters = clusters;
+        private MongoDBConfig config;
+
+        private String DEFALT_PROPERTIES = "mongo.yaml";
+
+        /**
+         * @description:若不提供默认配置路径，则加载默认文件
+         * @since: 2019-02-20
+         * @param configFile
+         * @return: com.ccue.datacenter.core.db.mongo.MongoDBContext.Builder
+         */
+        public Builder loadConfig(String configFile) {
+            if (configFile == null) {
+                configFile = DEFALT_PROPERTIES;
+            }
+            MongoDBConfig config = new YamlUtil.Reader().path("mongo.yaml").load().readAs(MongoDBConfig.class);
+            this.config = config;
             return this;
         }
+
         public MongoDBContext build() {
             return new MongoDBContext(this);
         }
@@ -28,7 +55,7 @@ public class MongoDBContext extends AbstractDatabaseContext {
 
     MongoDBContext(Builder builder) {
         super(builder);
-        clusters = builder.clusters;
+        config = builder.config;
     }
 
 
