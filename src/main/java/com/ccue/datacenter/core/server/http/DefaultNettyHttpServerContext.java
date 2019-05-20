48e10f844d5c6d51c9de4ec7c4b4d523bbf7f185
amzking <amzking@foxmail.com>
2019-05-20 10:49:23 +0800

    change

--- src/main/java/com/ccue/datacenter/core/server/http/DefaultNettyHttpServerContext.java
+++ src/main/java/com/ccue/datacenter/core/server/http/DefaultNettyHttpServerContext.java
@@ -1,6 +1,6 @@
 package com.ccue.datacenter.core.server.http;
 
-import com.ccue.datacenter.core.server.http.mvc.dispatch.IDispatcher;
+import com.ccue.datacenter.core.mvc.dispatch.IDispatcher;
 import com.ccue.datacenter.core.server.http.mvc.dispatch.NettyHttpDispatcher;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
--- src/main/java/com/ccue/datacenter/core/server/http/HttpServerContext.java
+++ src/main/java/com/ccue/datacenter/core/server/http/HttpServerContext.java
@@ -1,7 +1,7 @@
 package com.ccue.datacenter.core.server.http;
 
+import com.ccue.datacenter.core.mvc.dispatch.IDispatcher;
 import com.ccue.datacenter.core.server.ServerContext;
-import com.ccue.datacenter.core.server.http.mvc.dispatch.IDispatcher;
 
 public interface HttpServerContext<K, T> extends ServerContext {
 
